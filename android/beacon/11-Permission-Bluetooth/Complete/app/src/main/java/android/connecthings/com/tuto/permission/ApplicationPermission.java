cd5ed697ec4ace988538d2463c6b908513e3e7d4
olivier stevens <olivier.stevens@connecthings.com>
2016-12-19 10:46:01 +0100

    #UpdateVersion

--- android/beacon/11-Permission-Bluetooth/Complete/app/src/main/java/android/connecthings/com/tuto/permission/ApplicationPermission.java
+++ android/beacon/11-Permission-Bluetooth/Complete/app/src/main/java/android/connecthings/com/tuto/permission/ApplicationPermission.java
@@ -12,19 +12,15 @@ import com.connecthings.util.connection.Url;
  */
 public class ApplicationPermission extends Application{
 
-    private static final String UUID = "B0462602-CBF5-4ABB-87DE-B05340DCCBC5";
-
-
-
     public void onCreate(){
         super.onCreate();
-        AdtagInitializer.initInstance(this).initUrlType(Url.UrlType.PROD)
-                .initUser("**USER**", "**PSW**").initCompany("**COMPANY**");
+        AdtagInitializer.initInstance(this).initUrlType(Url.UrlType.__PLATFORM__)
+                .initUser("__user__", "__pws__").initCompany("__company__");
         //Initiate the adtagLogManager that manages the way log are sent to the platform
         AdtagLogsManager.initInstance(this, Network.ALL, 200, 1000 * 60 * 2);
         //If youe need more parameter - AdtagLogsManager.initInstance(this, Network.ALL,  50, 1000*60*2);
         //Initiate the beaconManager with the UUID of your beacons company. our beaconManager manage only one beacon Region based on the uuid
-        AdtagBeaconManager beaconManager = AdtagBeaconManager.initInstance(this, "**UUID**");
+        AdtagBeaconManager beaconManager = AdtagBeaconManager.initInstance(this, "__UUID__");
         beaconManager.setEnableBluetoothWhenAppInBackground(BluetoothManager.BLUETOOTH_BACKGROUND_ACTIVATION_STATUS.ASK_PERMISSION);
     }
 
