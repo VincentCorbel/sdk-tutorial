cc6eb8a28cd3f8662f47c7645556cd7d7b370e22
Ludovic Vimont <ludovic@Mac-mini.local>
2017-03-30 16:30:17 +0200

    #2.7: Update the Notification-Strategy/Complete with 2.7 features

--- android/beacon/6-Notification-Strategy/Complete/app/src/main/java/android/connecthings/com/tuto/notification/ApplicationNotification.java
+++ android/beacon/6-Notification-Strategy/Complete/app/src/main/java/android/connecthings/com/tuto/notification/ApplicationNotification.java
@@ -1,42 +1,28 @@
 package android.connecthings.com.tuto.notification;
 
 import android.app.Application;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+
 import com.connecthings.adtag.AdtagInitializer;
-import com.connecthings.adtag.analytics.AdtagLogsManager;
-import com.connecthings.adtag.model.sdk.BeaconContent;
-import com.connecthings.util.Log;
 import com.connecthings.util.adtag.beacon.AdtagBeaconManager;
-import com.connecthings.util.adtag.beacon.model.BeaconNotification;
-import com.connecthings.util.connection.Network;
 import com.connecthings.util.connection.Url;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.support.v4.app.NotificationCompat;
-import android.text.TextUtils;
 
-/**
- */
 public class ApplicationNotification extends Application {
+    private static final String TAG = ApplicationNotification.class.getSimpleName();
 
-    private static final String TAG = "ApplicationNotification";
-    private static final int NOTIFICATION_BEACON_ID = 1;
-    private NotificationManager mNotificationManager;
+    final String USER = "";
+    final String PASS = "";
+    final String COMPANY = "";
 
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
 
-        AdtagInitializer.initInstance(this).initUrlType(Url.UrlType.**PLATFORM**)
-                .initUser("**LOGIN**", "**PASSWORD**").initCompany("***COMPANY***");
-        //Initiate the adtagLogManager that manages the way log are sent to the platfor
-        AdtagLogsManager.initInstance(this, Network.ALL, 200, 1000 * 60 * 2);
-        //If you need more parameter - AdtagLogsManager.initInstance(this, Network.ALL,  50, 1000*60*2);
-        //Initiate the beaconManager with the UUID of your beacons company. our beaconManager manage only one beacon Region based on the uuid
-        AdtagBeaconManager beaconManager = AdtagBeaconManager.initInstance(this, "***UUID***");
-        //Waits 5 min before showing a new beacon notification
-        beaconManager.addNotificationStrategy(new BeaconNotificationStrategyFilter(1000 * 60 * 5));
+        AdtagInitializer
+                .initInstance(this)
+                .initUrlType(Url.UrlType.DEV)
+                .initUser(USER, PASS)
+                .initCompany(COMPANY)
+                .synchronize();
+
+        AdtagBeaconManager.getInstance().registerNotificationStrategy(new BeaconNotificationStrategyFilter(5 * 60 * 1000));
     }
-
-
 }
