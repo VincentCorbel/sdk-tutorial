01ef2ff96010b7b0e15930460ae62d710efa3c9f
stevensolivier <stevensolivier@connecthings.com>
2017-04-19 11:11:28 +0200

    #Swift #Strategy update and clean

---
+++ ios/ObjectiveC/Beacon/6-Notification-Strategy/Starter/NotificationStrategyComplete/BeaconNotificationStrategyFilter.m
@@ -0,0 +1,13 @@
+//
+//  BeaconNotificationStrategyFilter.m
+//  NotificationStrategyComplete
+//
+//  Created by Stevens Olivier on 19/04/2017.
+//  Copyright © 2017 sarra srairi. All rights reserved.
+//
+
+#import "BeaconNotificationStrategyFilter.h"
+
+@implementation BeaconNotificationStrategyFilter
+
+@end
