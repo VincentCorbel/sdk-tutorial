db7d1504085a93c7de32be8d2898f8df8cdfe6bb
Ludovic Vimont <ludovic@Mac-mini.local>
2017-04-21 17:48:30 +0200

    #Credentials: update script, ready for IOS [ObjectiveC]

--- credentials.sh
+++ credentials.sh
@@ -11,19 +11,51 @@ echo ""
 
 function updateURLType() { 
 	grep -rlE "initUrlType(.*)" android | xargs sed -i '' "s/\(.initUrlType(\).[^)]*\(.*\)/\1Url.UrlType.$1\2/g"
+
+	case "$1" in
+        LOCAL)
+			URLTYPE="ATUrlTypeLocal"
+            ;;
+        DEMO)
+			URLTYPE="ATUrlTypeDemo"
+            ;;
+        DEV)
+			URLTYPE="ATUrlTypeDev"
+            ;;
+        ITG)
+			URLTYPE="ATUrlTypeItg"
+            ;;
+        PRE_PROD)
+			URLTYPE="ATUrlTypePreprod"
+            ;;
+        PROD)
+			URLTYPE="ATUrlTypeProd"
+            ;;
+        PROD_US)
+			URLTYPE="ATUrlTypeProdUs"
+            ;;
+        *)
+			URLTYPE="ATUrlTypeDev"
+            ;;
+	esac
+
+	grep -rlE "configureUrlType:.*" ios/ObjectiveC | xargs sed -i '' "s/\(configureUrlType:\).[^ ]*\(.*\)/\1$URLTYPE\2/g"
 }
 
 function updateUser() { 
 	grep -rlE "initUser(.*)"  android | xargs sed -i '' "s/\(.initUser(\).[^)]*\(.*\)/\1\"$1\", \"$2\"\2/g"
+	grep -rlE "andLogin:.*" ios/ObjectiveC | xargs sed -i '' "s/\(andLogin:\).[^ ]*\(.*\)/\1@\"$1\"\2/g"
+	grep -rlE "andPassword:.*" ios/ObjectiveC | xargs sed -i '' "s/\(andPassword:\).[^ ]*\(.*\)/\1@\"$2\"\2/g"
 }
 
 function updateCompany() { 
 	grep -rlE "initCompany(.*)" android | xargs sed -i '' "s/\(.initCompany(\).[^)]*\(.*\)/\1\"$1\"\2/g"
+	grep -rlE "andCompany:.*" ios/ObjectiveC | xargs sed -i '' "s/\(andCompany:\).[^]]*\(.*\)/\1@\"$1\"\2/g"
 }
 
-# [[[ATAdtagInitializer sharedInstance] configureUrlType:__PLATFORM__ andLogin:@"__USER__" andPassword:@"__PSWD__" andCompany:@"__COMPANY__"] synchronize];
 # ATAdtagInitializer.sharedInstance().configureUrlType(__UrlType__, andLogin: "__USER__", andPassword: "__PSWD__", andCompany: "__COMPANY__").synchronize();
 
 updateURLType $urltype
 updateUser $user $pass
 updateCompany $company
+
