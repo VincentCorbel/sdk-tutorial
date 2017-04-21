4b04a8f66520cab3fabce50694a8496db8ed4ce3
Ludovic Vimont <ludovic@Mac-mini.local>
2017-04-21 17:11:00 +0200

    #Credentials: update script, ready for Android

--- credentials.sh
+++ credentials.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+export LC_CTYPE=C
+export LANG=C
+
 read -ep "User name? " user 
 read -ep "Password? " pass 
 read -ep "Company key? " company
+read -ep "UrlType (LOCAL, DEMO, DEV, ITG, PRE_PROD, PROD, PROD_US)? " urltype 
 echo ""
 
-function findAndReplace() {
-	grep -rl $1 "android" | xargs sed -i '' "s/$1/$2/g"
-	grep -rl $1 "ios" | xargs sed -i '' "s/$1/$2/g"
+function updateURLType() { 
+	grep -rlE "initUrlType(.*)" android | xargs sed -i '' "s/\(.initUrlType(\).[^)]*\(.*\)/\1Url.UrlType.$1\2/g"
 }
 
-findAndReplace "__LOGIN__" $user
-findAndReplace "__PSWD__" $pass
-findAndReplace "__COMPANY__" $company
+function updateUser() { 
+	grep -rlE "initUser(.*)"  android | xargs sed -i '' "s/\(.initUser(\).[^)]*\(.*\)/\1\"$1\", \"$2\"\2/g"
+}
+
+function updateCompany() { 
+	grep -rlE "initCompany(.*)" android | xargs sed -i '' "s/\(.initCompany(\).[^)]*\(.*\)/\1\"$1\"\2/g"
+}
+
+# [[[ATAdtagInitializer sharedInstance] configureUrlType:__PLATFORM__ andLogin:@"__USER__" andPassword:@"__PSWD__" andCompany:@"__COMPANY__"] synchronize];
+# ATAdtagInitializer.sharedInstance().configureUrlType(__UrlType__, andLogin: "__USER__", andPassword: "__PSWD__", andCompany: "__COMPANY__").synchronize();
+
+updateURLType $urltype
+updateUser $user $pass
+updateCompany $company
