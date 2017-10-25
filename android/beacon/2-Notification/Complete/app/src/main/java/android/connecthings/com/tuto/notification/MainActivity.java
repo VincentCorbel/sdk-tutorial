1425cfb99abf69eafba16ca94cd2209570a4704d
Ludovic Vimont <>
2017-10-25 16:15:44 +0200

    #Notification: added getIntent extras

--- android/beacon/2-Notification/Complete/app/src/main/java/android/connecthings/com/tuto/notification/MainActivity.java
+++ android/beacon/2-Notification/Complete/app/src/main/java/android/connecthings/com/tuto/notification/MainActivity.java
@@ -2,11 +2,26 @@ package android.connecthings.com.tuto.notification;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.connecthings.connectplace.actions.model.PlaceNotification;
+import com.connecthings.connectplace.actions.notification.NotificationManager;
+import com.connecthings.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getIntent().getExtras() != null) {
+            if (getIntent().getExtras().containsKey(NotificationManager.NOTIFICATION_CONTENT)) {
+                PlaceNotification placeNotification = getIntent().getExtras().getParcelable(NotificationManager.NOTIFICATION_CONTENT);
+                Log.d(TAG, placeNotification);
+                ((TextView) findViewById(R.id.beacon_content)).setText(getString(R.string.beacon_content));
+            }
+        }
     }
 }
